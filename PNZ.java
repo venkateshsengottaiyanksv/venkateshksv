52791f926cc8da1b9c1fcde67136d3636c44f184
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-12 10:14:01 +0530

    Add files via upload

---
+++ PNZ.java
@@ -0,0 +1,17 @@
+package ksv;
+import java.util.Scanner;
+public class PNZ{
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int n;
+	Scanner ss=new Scanner(System.in);
+	n=ss.nextInt();
+	if(n>0){
+		System.out.println("positive");}
+	else if(n>0){
+		System.out.println("negative");}
+	else{
+		System.out.println("zero");
+	}
+	 
+	  }}  
