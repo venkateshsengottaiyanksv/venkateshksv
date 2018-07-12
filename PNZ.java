5f1437da78580f4d6e1946441b1f6407ea2b6d9c
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-12 10:40:15 +0530

    Add files via upload

--- PNZ.java
+++ PNZ.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
 	n=ss.nextInt();
 	if(n>0){
 		System.out.println("positive");}
-	else if(n>0){
+	else if(n<0){
 		System.out.println("negative");}
 	else{
 		System.out.println("zero");
