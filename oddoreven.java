7805fc011f0ad1e9fb96f305c78db7b27c2f2b4c
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-12 11:52:25 +0530

    Add files via upload

---
+++ oddoreven.java
@@ -0,0 +1,18 @@
+package ksv;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class oddoreven {
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int n;
+   Scanner ss=new Scanner(System.in);
+   n=ss.nextInt();
+   if(n%2==0){
+	   System.out.println("even");
+   }else{
+	   System.out.println("odd");
+   }
+  }
+  
+   
+}
