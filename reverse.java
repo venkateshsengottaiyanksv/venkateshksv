abbd982af79c61755b2a054a70f10507fdd3d19e
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-13 18:05:59 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,14 @@
+package ksvp;
+
+import java.util.Scanner;
+
+public class reverse {
+public static void main(String[] args){
+	String s;
+	Scanner ss= new Scanner(System.in);
+	s = ss.nextLine();
+	StringBuffer ss1=new StringBuffer(s);
+	System.out.println(ss1.reverse());
+	
+}
+}
