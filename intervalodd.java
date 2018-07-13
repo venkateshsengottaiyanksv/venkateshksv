e61d02093b5c64da9786ca5fef9c8a87acd46bcf
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-13 17:17:03 +0530

    Add files via upload

---
+++ intervalodd.java
@@ -0,0 +1,15 @@
+package ksvp;
+
+import java.util.Scanner;
+class intervalodd {
+    public static void main(String[] args) {
+      int n,a,i;
+      Scanner scan = new Scanner(System .in);
+      n = scan.nextInt();
+      a = scan.nextInt();
+      for(i=n;i<a;i++){
+        if(i%2!=0){	  
+    	  System.out.println(i);
+      }}
+  }
+}
