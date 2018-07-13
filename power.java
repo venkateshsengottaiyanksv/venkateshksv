aef3707d444c14c4583d85f4259e9e15d31c42f3
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-13 10:53:05 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,17 @@
+package ksv;
+
+import java.util.Scanner;
+
+public class power {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a,b;
+Scanner scan = new Scanner( System.in);
+a = scan.nextInt();
+b = scan.nextInt();
+System.out.println((int)Math.pow(a,b));
+
+	}
+
+}
