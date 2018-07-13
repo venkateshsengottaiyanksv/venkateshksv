f4642aa9640b6ab559fb2835efc959a5770c342d
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-13 09:53:25 +0530

    Add files via upload

---
+++ sumnatural.java
@@ -0,0 +1,15 @@
+package ksv;
+import java.util.Scanner;
+public class sumnatural {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int a,i,b=0;
+  Scanner sc = new Scanner (System .in);
+  a = sc . nextInt();
+  for(i=0;i<=a;i++){
+	  b=b+i;
+  }System.out.println(b);
+	}
+
+}
