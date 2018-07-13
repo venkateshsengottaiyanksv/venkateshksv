e3e502536dff74d3336bddf70e78fb5be080594c
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-13 10:35:17 +0530

    Add files via upload

---
+++ countnum.java
@@ -0,0 +1,13 @@
+package ksv;
+
+import java.util.Scanner;
+
+public class countnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Scanner scan = new Scanner(System.in);
+  int n=scan.nextInt();
+  System.out.println(String.valueOf(n).length());
+}
+}
