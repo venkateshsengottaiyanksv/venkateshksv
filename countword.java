058b2a411d01e8f7e8c7850658045fd79de2b7ea
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-13 09:38:00 +0530

    Add files via upload

---
+++ countword.java
@@ -0,0 +1,18 @@
+package ksv;
+
+import java.util.Scanner;
+
+public class countword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int n,i;
+   String s="hello";
+   Scanner scan = new Scanner (System.in);
+   n = scan.nextInt();
+   for(i=0;i<n;i++){
+	   System.out.println(s);
+   }
+	}
+
+}
