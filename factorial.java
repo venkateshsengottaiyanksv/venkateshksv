177e49a61d07e54bade88efb641690a3388d2b51
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-14 09:29:28 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,18 @@
+package ksvp;
+
+import java.util.Scanner;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int n,i,a=1;
+  Scanner scan= new Scanner(System.in);
+  n = scan.nextInt();
+  for(i=1;i<=n;i++){
+	a=a*i;}
+		
+	System.out.println(a);
+  }}
+
+
