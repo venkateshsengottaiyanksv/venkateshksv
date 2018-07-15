62f1347b23bf74671b30871da16fcf18b7f9f177
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-15 10:04:34 +0530

    Add files via upload

---
+++ Stringappend.java
@@ -0,0 +1,13 @@
+package ksvp;
+import java.util.Scanner;
+public class Stringappend {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  String s;
+  Scanner scan=new Scanner(System.in);
+  s=scan.nextLine();
+  StringBuffer sb=new StringBuffer(s);
+  System.out.println(sb.append(""));
+}	}
+
+
