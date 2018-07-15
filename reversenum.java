5c42bcae375a851f0689e46941eff3a368503d3d
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-15 09:35:44 +0530

    Add files via upload

---
+++ reversenum.java
@@ -0,0 +1,12 @@
+package ksvp;
+import java.util.Scanner;
+public class reversenum {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  String a;
+  Scanner scan=new Scanner(System.in);
+  a=scan.nextLine();
+  StringBuffer s=new StringBuffer(a);
+   System.out.println(s.reverse());}
+
+}
