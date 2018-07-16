a03f75d7d2ee8fc16d5404d700d6bcb2a054adc4
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-16 18:21:45 +0530

    Add files via upload

---
+++ nonrepeatednumber.java
@@ -0,0 +1,28 @@
+package ksvp;
+import java.util.Scanner;
+public class nonrepeatednumber {
+   public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int a,i,j,k=0,cnt=0;
+ String b="";
+ Scanner ss= new Scanner(System.in);
+ a = ss. nextInt();
+ int arr[]=new int[a];
+ for(i=0;i<a;i++){
+	 arr[i]=ss.nextInt();
+	 }
+ for(i=0;i<a;i++){
+	 for(j=i+1,cnt=0;j<a;j++){
+	 if(arr[i]==arr[j]){
+		 cnt++;
+		 for(k=j;k<a-1;k++) arr[k]=arr[k+1];
+		 a--;
+		 j--;
+	 }
+	 }
+	 if(cnt==0)System.out.println(arr[i]);
+ }
+   }
+}
+
+
