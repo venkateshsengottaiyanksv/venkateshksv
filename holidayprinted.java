ddac25e736aa95ac49c50eb471f1fed1cd4f08c0
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-17 14:30:11 +0530

    Add files via upload

---
+++ holidayprinted.java
@@ -0,0 +1,18 @@
+package ksvp;
+import java.util.Scanner;
+public class holidayprinted {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s;
+		Scanner scan = new Scanner(System.in);
+		s = scan.nextLine().intern();
+		if(s=="sunday"||s=="saturday"){
+			System.out.println("yes");
+		}else{
+			System.out.println("No");
+		}}}
+
+	
+
+
