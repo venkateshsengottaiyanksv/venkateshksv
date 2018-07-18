11cce3097382e99c15d5c1e7677bb49d2d331bc0
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-18 14:01:34 +0530

    Add files via upload

---
+++ mutilpletables.java
@@ -0,0 +1,15 @@
+package ksvp;
+import java.util.Scanner;
+public class mutilpletables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int a,i,b=0;
+   Scanner scan=new Scanner(System.in);
+   a =scan. nextInt();
+   for(i=1;i<=5;i++){
+	  b=i*a;
+   System.out.println(b+"");}
+	}
+
+}
