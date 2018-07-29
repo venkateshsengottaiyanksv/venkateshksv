864e25c689d0bf84476a466c45614f063a6d5357
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-07-29 14:18:19 +0530

    Add files via upload

---
+++ swappingbitwiseoperator.java
@@ -0,0 +1,18 @@
+package ksvp;
+import java.util.Scanner;
+public class swappingbitwiseoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ss = new Scanner (System.in);
+		int a= ss.nextInt();
+		int b=ss.nextInt();
+  a=a^b;
+  b=a^b;
+  a=a^b;
+  System.out.println(a+" "+b);
+		
+
+	}
+
+}
