79e1fb35f7c9a52ee7d8882f6d0f27cbdbb921a1
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-08-02 09:34:22 +0530

    Add files via upload

---
+++ spacecalculate.java
@@ -0,0 +1,13 @@
+package ksvp;
+import java.util.Scanner;
+public class spacecalculate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Scanner scan = new Scanner (System .in);
+ String s  = scan .nextLine();
+ String s1[] = s .split(" ");
+ System.out.println(s1.length);
+	}
+
+}
