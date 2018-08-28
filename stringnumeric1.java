4f5a54a1bbc4a1419a60c7da256f1cb66c9f3ad1
venkateshsengottaiyanksv <41136583+venkateshsengottaiyanksv@users.noreply.github.com>
2018-08-28 10:03:34 +0530

    Add files via upload

---
+++ stringnumeric1.java
@@ -0,0 +1,20 @@
+package ksvp;
+import java.util.Scanner;
+public class stringnumeric1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    String a;int x=0,y=0;
+    Scanner scan=new Scanner(System.in);
+    a= scan .nextLine();
+      x = a.length();
+     String s = a.replaceAll("[a-zA-Z]","");
+     y = s.length();
+     if(x==y){
+    	System.out.println("yes");
+    }else{
+    	System.out.println("no");
+    }
+}
+	}
+
